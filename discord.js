117536dcff1f521ebcfa669d648174e7b7618632
ciomeczek <ciomek05@gmail.com>
2021-01-07 22:48:22 +0100

    update

--- discord.js
+++ discord.js
@@ -3,16 +3,30 @@ const randomFile = require('select-random-file')
 require('dotenv').config()
 const client = new Discord.Client()
 
+const animeMsg = ['jebać anime', 'jebac anime', 'jevac anime', 'jebać animu', 'jebac animu', 'j e b a c a n i m e']
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
-    if (msg.content.toLowerCase().includes('jebać anime') || msg.content.toLowerCase().includes('jebac anime')) {
-        randomFile("./Anime", (err, file) => {
-            if (err) throw err
-            msg.reply("Ciebie też", { files: ["./Anime/" + file] })
-        })
+    //const cuttedMsg = msg.content.toLowerCase().includes('jebać anime')
+
+    for (let element of animeMsg) {
+        if (msg.content.toLowerCase().includes(element)) {
+            randomFile("./Anime", (err, file) => {
+                if (err) throw err
+                msg.reply("ciebie też", { files: ["./Anime/" + file] })
+            })
+        }
+        break
     }
+
+    // if (cuttedMsg === 'jebać anime' || cuttedMsg === 'jebac anime') {
+    //     randomFile("./Anime", (err, file) => {
+    //         if (err) throw err
+    //         msg.reply("Ciebie też", { files: ["./Anime/" + file] })
+    //     })
+    // }
 })
 client.login(process.env.BOT_TOKEN)
