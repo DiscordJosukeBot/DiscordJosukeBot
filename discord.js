0fc075f8bcd6f0f401db0224d12db04d5ab64fe5
ciomeczek <ciomek05@gmail.com>
2021-01-11 22:02:04 +0100

    no gays

--- discord.js
+++ discord.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const client = new Discord.Client()
 
 const animeMsg = ['jebać anime', 'jebac anime', 'jevac anime', 'jebać animu', 'jebac animu', 'j e b a c a n i m e', 'j e b a c     a n i m e']
+const noAnime = ['no anime', 'n o a n i m e']
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -15,6 +16,16 @@ client.on('message', msg => {
             randomFile("./Anime", (err, file) => {
                 if (err) throw err
                 msg.reply("Ciebie też", { files: ["./Anime/" + file] })
+            })
+            return false
+        }
+        else return true
+    })
+    noAnime.every((element, index) => {
+        if (msg.content.toLowerCase().includes(element)) {
+            randomFile("./Anime", (err, file) => {
+                if (err) throw err
+                msg.reply("No gays", { files: ["./Anime/" + file] })
             })
             return false
         }
